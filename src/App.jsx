79be4af7035237b045af93f6b8cd11e08779e394
princerajkr45 <priyance5204@gmail.com>
2024-06-08 00:55:05 +0530

    wrap color select button

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         style={{ backgroundColor: color }}
       >
         <div className='flex justify-center  fixed bottom-12 inset-x-0 px-2'>
-          <div className='flex justify-center gap-3 bg-white px-3 py-2 rounded-3xl'>
+          <div className='flex flex-wrap justify-center gap-3 bg-white px-3 py-2 rounded-3xl'>
             <button className='outline-none rounded-full px-4 py-1 text-white'
             style={{ backgroundColor: "red" }}
             onClick={() => setColor("red")}
